d8045b3f759e5185c24db30cebd377d1ad380a28
Peter Eisentraut <peter@eisentraut.org>
2023-12-01 11:20:37 +0100

    Add some example data to test

--- src/test/regress/sql/graph_table.sql
+++ src/test/regress/sql/graph_table.sql
@@ -52,7 +52,38 @@ SELECT customer_name FROM GRAPH_TABLE (pg_class MATCH (c IS customers WHERE addr
 SELECT customer_name FROM GRAPH_TABLE (myshop MATCH (c IS customers WHERE address = 'US')-[IS customer_orders]->(o IS orders) COLUMNS (cx.name AS customer_name));  -- error
 SELECT customer_name FROM GRAPH_TABLE (myshop MATCH (c IS customers WHERE address = 'US')-[IS customer_orders]->(o IS orders) COLUMNS (c.namex AS customer_name));  -- error
 
+INSERT INTO products VALUES
+    (1, 'product1', 10),
+    (2, 'product2', 20),
+    (3, 'product3', 30);
+INSERT INTO customers VALUES
+    (1, 'customer1', 'US'),
+    (2, 'customer2', 'CA'),
+    (3, 'customer3', 'GL');
+INSERT INTO orders VALUES
+    (1, '2024-01-01'),
+    (2, '2024-01-02'),
+    (3, '2024-01-03');
+INSERT INTO order_items (order_items_id, order_id, product_no, quantity) VALUES
+    (1, 1, 1, 5),
+    (2, 1, 2, 10),
+    (3, 2, 1, 7);
+INSERT INTO customer_orders (customer_orders_id, customer_id, order_id) VALUES
+    (1, 1, 1),
+    (2, 2, 2);
+
 SELECT customer_name FROM GRAPH_TABLE (myshop MATCH (c IS customers WHERE address = 'US')-[IS customer_orders]->(o IS orders) COLUMNS (c.name AS customer_name));  -- TODO
+
+-- TODO: should approximately match this query:
+SET debug_print_parse = on;
+SELECT customer_name FROM (
+    SELECT c.name AS customer_name
+    FROM customers c, customer_orders _co, orders o
+    WHERE _co.customer_id = c.customer_id
+      AND _co.order_id = o.order_id
+      AND c.address = 'US'
+) myshop;
+RESET debug_print_parse;
 
 CREATE VIEW customers_us AS SELECT customer_name FROM GRAPH_TABLE (myshop MATCH (c IS customers WHERE address = 'US')-[IS customer_orders]->(o IS orders) COLUMNS (c.name AS customer_name));
 
