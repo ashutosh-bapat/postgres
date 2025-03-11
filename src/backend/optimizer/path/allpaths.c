67ac141ed9d63a0ea8a5be80b92327ed5c2d9e45
Junwang Zhao <zhjwpku@gmail.com>
2025-03-11 12:15:03 +0000

    Handle RTE_GRAPH_TABLE in more places
    The commit is result of assessing again all places where
    RangeTblEntry::rtekind is used. Most of the places have been taken care
    of by Peter in his earlier patches. There are still a few places that
    need a change:
    
    1. In AcquireRewriteLocks lock property graph being queries by adding
       RTE_GRAPH_TABLE similar to RTE_RELATION. Without this change an
       assertion added by commit 525392d crashes. The commit has been
       reverted now. So it doesn't crash without this change anymore. But
       the change looks correct. Hence kept it.
    
       Discussion: https://www.postgresql.org/message-id/CA+HiwqH7mMiTAzP+JtdRnNuToK0u0P7kf+De6e79MhZcPSMhMA@mail.gmail.com
       Author: Amit Langote
       Reported by: Junwang Zhou
    
    2. Handle RTE_GRAPH_TABLE in print_rt(): the function is not probably
       maintained and may get removed. But while it's still there let's keep
       it upto date.
    
       Relevant discussion: https://www.postgresql.org/message-id/flat/7d023c20-6679-44bd-b5f7-44106659bd5a%40eisentraut.org.
    
    3. The change in set_rel_consider_parallel() just puts the
       RTE_GRAPH_CASE next to RTE_GROUP, which is also a "shouldn't happen"
       case.
    
    4. Change a comment in ScanQueryForLocks() to mention property graphs
       along with view.
    
    Author: Ashutosh Bapat

--- src/backend/optimizer/path/allpaths.c
+++ src/backend/optimizer/path/allpaths.c
@@ -728,13 +728,16 @@ set_rel_consider_parallel(PlannerInfo *root, RelOptInfo *rel,
 			 */
 			return;
 
-		case RTE_GRAPH_TABLE:
-			/* shouldn't happen here */
-			break;
-
 		case RTE_RESULT:
 			/* RESULT RTEs, in themselves, are no problem. */
 			break;
+
+		case RTE_GRAPH_TABLE:
+			/*
+			 * Shouldn't happen since these are replaced by subquery RTEs when
+			 * rewriting queries.
+			 */
+			/* FALLTHROUGH */
 		case RTE_GROUP:
 			/* Shouldn't happen; we're only considering baserels here. */
 			Assert(false);
