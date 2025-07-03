4176053f803211dc260f5369b8c9756c9ffd5e0c
Ashutosh Bapat <ashutosh.bapat.oss@gmail.com>
2025-07-03 13:44:43 +0530

    Property graphs in pg_overexplain
    Property graphs introduce a new kind of range table entry (RTE) and a new
    relation kind. This commit updates the pg_overexplain extension to handle these
    new kinds correctly.

--- contrib/pg_overexplain/pg_overexplain.c
+++ contrib/pg_overexplain/pg_overexplain.c
@@ -432,7 +432,13 @@ overexplain_range_table(PlannedStmt *plannedstmt, ExplainState *es)
 				kind = "group";
 				break;
 			case RTE_GRAPH_TABLE:
-				kind = "property graph";
+				/*
+				 * We should not see RTE of this kind here since property graph
+				 * RTE gets converted to subquery RTE in RewriteGraphTable().
+				 * In case we decide not to do the conversion and leave RTEkind
+				 * unchanged in future, print correct name of RTE kind.
+				 */
+				kind = "property_graph";
 				break;
 		}
 
@@ -524,6 +530,9 @@ overexplain_range_table(PlannedStmt *plannedstmt, ExplainState *es)
 				break;
 			case RELKIND_PARTITIONED_INDEX:
 				relkind = "partitioned_index";
+				break;
+			case RELKIND_PROPGRAPH:
+				relkind = "property_graph";
 				break;
 			case '\0':
 				relkind = NULL;
@@ -646,6 +655,12 @@ overexplain_range_table(PlannedStmt *plannedstmt, ExplainState *es)
 			ExplainPropertyText("ENR Name", rte->enrname, es);
 			ExplainPropertyFloat("ENR Tuples", NULL, rte->enrtuples, 0, es);
 		}
+
+		/*
+		 * rewriteGraphTable() clears graph_pattern and graph_table_columns
+		 * fields, so skip them. No graph table specific fields are required to
+		 * be printed.
+		 */
 
 		/*
 		 * add_rte_to_flat_rtable will clear groupexprs and securityQuals, so
